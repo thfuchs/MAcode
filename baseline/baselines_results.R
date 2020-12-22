c7acd9350029eec3e77af451908e4e2873c64422
TFcfgo <thomas.fuchs@cfgo.de>
2020-12-22 14:38:24 +0100

    Update data

--- baseline/baselines_results.R
+++ baseline/baselines_results.R
@@ -1,49 +1,51 @@
-# Forecasts and Evaluations for Baseline Models for dow30 data set
+# Forecasts and Evaluations for Baseline Models
 
 ### Settings -------------------------------------------------------------------
+library(data.table)
+library(future)
+library(furrr)
+library(tsRNN)
 
-data <- tsRNN::dow30_clean
+data_ebit <- readRDS("data/data_ebit.rds")[, .(ticker, index, value)]
+dummies <- readRDS("data/dummies.rds")[ticker %in% unique(data_ebit$ticker)]
 
 cv_setting <- list(
-  periods_train = 40,
+  periods_train = 36,
   periods_val = 6,
   periods_test = 6,
-  skip_span = 5
+  skip_span = 3
 )
 
-companies <- unique(data$ticker)
 multiple_h <- list(short = 1, medium = 1:4, long = 5:6, total = 1:6)
 
-
 ### EBIT -----------------------------------------------------------------------
-# library(future)
-# plan(multisession)
-#
-# forecast <- furrr::future_map(
-#   companies,
-#   purrr::possibly(function(x) {
-#     d <- data[ticker == x]
-#     predict_baselines(d, cv_setting, multiple_h = multiple_h)
-#   }, otherwise = NULL),
-#   .options = furrr::furrr_options(seed = 123) # seed for bootstrapped based PI
-# )
-#
-# message <- "Baseline estimation finished!"
-# content <- jsonlite::toJSON(list(list(
-#   type = "section",
-#   text = list(type = "mrkdwn", text = message)
-# )), auto_unbox = TRUE)
-#
-# bin <- httr::POST(
-#   url = 'https://slack.com/api/chat.postMessage',
-#   body = list(token = Sys.getenv("SLACK_BOT"),
-#               channel = Sys.getenv("SLACK_CHANNEL"), `blocks` = paste(content))
-# )
-#
-# fc_baselines_ebit <- purrr::compact(forecast)
-# str(fc_baselines_ebit, max.level = 1)
-#
-# save(fc_baselines_ebit, file = "baseline/fc_baselines_ebit.rda")
+plan(multisession)
+
+forecast <- furrr::future_map(
+  dummies$ticker,
+  purrr::possibly(function(x) {
+    d <- data_ebit[ticker == x]
+    predict_baselines(d, cv_setting, multiple_h = multiple_h)
+  }, otherwise = NULL),
+  .options = furrr::furrr_options(seed = 123) # seed for bootstrapped based PI
+)
+
+message <- "Baseline estimation finished!"
+content <- jsonlite::toJSON(list(list(
+  type = "section",
+  text = list(type = "mrkdwn", text = message)
+)), auto_unbox = TRUE)
+
+bin <- httr::POST(
+  url = 'https://slack.com/api/chat.postMessage',
+  body = list(token = Sys.getenv("SLACK_BOT"),
+              channel = Sys.getenv("SLACK_CHANNEL"), `blocks` = paste(content))
+)
+
+fc_baselines_ebit <- purrr::compact(forecast)
+str(fc_baselines_ebit, max.level = 1)
+
+save(fc_baselines_ebit, file = "baseline/fc_baselines_ebit.rda")
 load(file = "baseline/fc_baselines_ebit.rda")
 
 # Accuracy Measures
