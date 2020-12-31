caf4511f25ba6cacade2b4749d68d43218883406
thfuchs <thomasfuchs-re@web.de>
2020-12-31 11:43:35 +0000

    Finalise job 3

--- 03_rnn/simple/job_simple_ebit_03.R
+++ 03_rnn/simple/job_simple_ebit_03.R
@@ -13,20 +13,30 @@ source("settings.R")
 fc_ebit_rnn_bayes <- readRDS("03_rnn/simple/fc_ebit_rnn_bayes.rds")
 fc_ebit_rnn_prediction <- readRDS("03_rnn/simple/fc_ebit_rnn_prediction.rds")
 
+stopifnot(all(names(fc_ebit_rnn_bayes) == names(fc_ebit_rnn_prediction)))
+
+companies <- names(fc_ebit_rnn_prediction)
 multiple_h <- list(short = 1, medium = 1:4, long = 5:6, total = 1:6)
 
 ### Job ------------------------------------------------------------------------
-results <- tune_keras_rnn_eval(
-  fc_sample = fc_ebit_rnn_prediction.rds,
-  cv_setting = cv_setting_test,
-  bayes_best_par = purrr::map(fc_ebit_rnn_bayes, "Best_Par"),
-  col_id = NULL, col_date = "index", col_value = "value",
-  multiple_h = multiple_h,
-  frequency = 4,
-  level = 95
+results <- furrr::future_map(
+  companies,
+  purrr::possibly(function(x) {
+    fc <- fc_ebit_rnn_prediction[[x]]
+    bayes <- fc_ebit_rnn_bayes[[x]]
+    tune_keras_rnn_eval(
+      fc_sample = fc,
+      cv_setting = cv_setting_test,
+      bayes_best_par = purrr::map(bayes, "Best_Par"),
+      col_id = NULL, col_date = "index", col_value = "value",
+      multiple_h = multiple_h,
+      frequency = 4,
+      level = 95
+    )
+  }, otherwise = NULL, quiet = FALSE)
 )
 
-fc_ebit_rnn_eval <- purrr::compact(results)
+fc_ebit_rnn_eval <- purrr::compact(purrr::set_names(results, companies))
 
 # Success / Failure message
 if (length(purrr::compact(fc_ebit_rnn_eval)) > 0) {
