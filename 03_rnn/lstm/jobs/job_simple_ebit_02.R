74fe63924ce6597084854f7fcec5a65bb433790f
thfuchs <thomasfuchs-re@web.de>
2021-01-04 15:10:04 +0000

    copied simple rnn directory to get LSTM and GRU jobs

---
+++ 03_rnn/lstm/jobs/job_simple_ebit_02.R
@@ -0,0 +1,58 @@
+############################# job_lstm_ebit_02.R ###############################
+
+################################################################################
+### Training and forecasting on cross-validation splits with tuned parameters ##
+################################################################################
+
+### Settings -------------------------------------------------------------------
+rm(list = ls())
+
+source("utils.R")
+source("settings.R")
+
+data_ebit <- readRDS("data/data_ebit.rds")[, .(ticker, index, value)]
+companies <- unique(data_ebit$ticker)
+overall <- trunc(length(companies) / cores)
+fc_ebit_rnn_bayes <- readRDS("03_rnn/lstm/results/fc_ebit_rnn_bayes.rds")
+
+stopifnot(all(companies == names(fc_ebit_rnn_bayes)))
+
+### Job ------------------------------------------------------------------------
+forecast <- furrr::future_map(
+  companies,
+  purrr::possibly(function(x) {
+    d <- data_ebit[ticker == x]
+    current <- which(companies == x)
+    if (current <= overall) {
+      resp <- toSlack(paste0(
+        "LSTM EPS Bayes Optimization: Starting ", x, "\n",
+        round(current/overall * 100, 2), "% (", current, "/", overall, ")"
+      ))
+    }
+    bayes <- fc_ebit_rnn_bayes[[x]]
+    toSlack(paste0(
+      "Start LSTM EBIT Prediction for ",
+      x, " (", which(companies == x), "/", length(companies), ")"))
+    tune_keras_rnn_predict(
+      data = d,
+      col_id = "ticker",
+      col_date = "index",
+      col_value = "value",
+      model_type = "lstm",
+      cv_setting = cv_setting_test,
+      bayes_best_par = purrr::map(bayes, "Best_Par"),
+      iter_dropout = 500,
+      save_model = NULL
+      # save_model_id = "ebit"
+    )
+  }, otherwise = NULL, quiet = FALSE),
+  .options = furrr::furrr_options(seed = 123)
+)
+
+fc_ebit_rnn_prediction <- purrr::compact(purrr::set_names(forecast, companies))
+
+# Save and send Success / Failure message
+if (length(fc_ebit_rnn_prediction) > 0) {
+  saveRDS(fc_ebit_rnn_prediction, file = "03_rnn/lstm/results/fc_ebit_rnn_prediction.rds", compress = "xz")
+  toSlack("LSTM EBIT prediction finished")
+} else toSlack("Error: LSTM EBIT prediction failed")
---
+++ 03_rnn/lstm/jobs/job_simple_eps_02.R
@@ -0,0 +1,58 @@
+############################## job_lstm_eps_02.R ###############################
+
+################################################################################
+### Training and forecasting on cross-validation splits with tuned parameters ##
+################################################################################
+
+### Settings -------------------------------------------------------------------
+rm(list = ls())
+
+source("utils.R")
+source("settings.R")
+
+data_eps <- readRDS("data/data_eps.rds")[, .(ticker, index, value)]
+companies <- unique(data_eps$ticker)
+overall <- trunc(length(companies) / cores)
+fc_eps_rnn_bayes <- readRDS("03_rnn/lstm/results/fc_eps_rnn_bayes.rds")
+
+stopifnot(all(companies == names(fc_eps_rnn_bayes)))
+
+### Job ------------------------------------------------------------------------
+forecast <- furrr::future_map(
+  companies,
+  purrr::possibly(function(x) {
+    d <- data_eps[ticker == x]
+    current <- which(companies == x)
+    if (current <= overall) {
+      resp <- toSlack(paste0(
+        "LSTM EPS Bayes Optimization: Starting ", x, "\n",
+        round(current/overall * 100, 2), "% (", current, "/", overall, ")"
+      ))
+    }
+    bayes <- fc_eps_rnn_bayes[[x]]
+    toSlack(paste0(
+      "Start LSTM EPS Prediction for ",
+      x, " (", which(companies == x), "/", length(companies), ")"))
+    tune_keras_rnn_predict(
+      data = d,
+      col_id = "ticker",
+      col_date = "index",
+      col_value = "value",
+      model_type = "lstm",
+      cv_setting = cv_setting_test,
+      bayes_best_par = purrr::map(bayes, "Best_Par"),
+      iter_dropout = 500,
+      save_model = NULL
+      # save_model_id = "eps"
+    )
+  }, otherwise = NULL, quiet = FALSE),
+  .options = furrr::furrr_options(seed = 123)
+)
+
+fc_eps_rnn_prediction <- purrr::compact(purrr::set_names(forecast, companies))
+
+# Save and send Success / Failure message
+if (length(fc_eps_rnn_prediction) > 0) {
+  saveRDS(fc_eps_rnn_prediction, file = "03_rnn/lstm/results/fc_eps_rnn_prediction.rds", compress = "xz")
+  toSlack("LSTM EPS prediction finished")
+} else toSlack("Error: LSTM EPS prediction failed")
