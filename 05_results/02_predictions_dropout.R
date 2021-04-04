3ecac46682dc101cc760ca3d3cf127fc0b1f7f5b
TFcfgo <thomas.fuchs@cfgo.de>
2021-04-04 20:43:19 +0200

    slight update on predictions_dropout

--- 05_results/02_predictions_dropout.R
+++ 05_results/02_predictions_dropout.R
@@ -5,8 +5,8 @@ library(keras)
 aws.s3::bucketlist()
 aws.s3::bucket_exists(bucket = Sys.getenv("AWS_BUCKET"))
 
-s3_fun <- function(model, data, company, slice) {
-  file <- file.path(model, "models", data, paste(data, model, company, paste0("Slice", slice), "01.hdf5", sep = "_"))
+s3_fun <- function(model, data, company, split) {
+  file <- file.path(model, "models", data, paste(data, model, company, paste0("Slice", split), "01.hdf5", sep = "_"))
   if (!isTRUE(aws.s3::object_exists(object = file, bucket = Sys.getenv("AWS_BUCKET")))) {
     message("No File")
     return(NULL)
@@ -38,18 +38,23 @@ rate_data <- data.table(
   ticker = rep(dummies$ticker, each = 3*3*9),
   id = rep(c("ebit", "ni", "eps"), 481, each = 3*9),
   type = rep(c("simple", "lstm", "gru"), 481*3, each = 9),
-  slice = rep(1:9, 481*3*3)
+  split = rep(1:9, 481*3*3)
 )
 
 rate_data[, dropout := purrr::pmap(
-  list(ticker, id, type, slice),
-  purrr::possibly(function(f_ticker, f_id, f_type, f_slice) s3_fun(
+  list(ticker, id, type, split),
+  purrr::possibly(function(f_ticker, f_id, f_type, f_split) s3_fun(
     model = f_type,
     data = f_id,
     company = f_ticker,
-    slice = f_slice
+    split = f_split
   ), otherwise = NA_real_, quiet = FALSE)
 )]
+
+rate_data[, dropout := as.numeric(dropout)]
+rate_data[, id := factor(id, levels = c("ebit", "ni", "eps"), labels = c("EBIT", "Net Income", "EPS"))]
+rate_data[, type := factor(type, levels = c("simple", "lstm", "gru"), labels = c("Simple", "LSTM", "GRU"))]
+
 saveRDS(rate_data, "05_results/predictions_dropout.rds", compress = "xz")
 
-# s3_fun(model = "simple", data = "ni", company = "RAND_NA", slice = 3)
+# s3_fun(model = "simple", data = "ni", company = "RAND_NA", split = 3)
